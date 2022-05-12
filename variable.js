0d9be93c2a19e91d5206a668bfa3677e839591bf
Muqeetmirza <a.muqet9405@gmail.com>
2022-05-12 16:56:31 +0500

    variable js

---
+++ variable.js
@@ -0,0 +1,18 @@
+//simple variable declare
+var a=10,b=13,c;
+c=a+b;
+console.log(c);
+
+var d=25,e=65,f;
+f=d*e;
+console.log(f);
+
+//text adding method
+var na="M.", lastname="haider",middlename="Ali"
+na=na+middlename +lastname
+console.log(na)
+
+//Let with constant
+const m=7654, n=67354;
+let total=m*n;
+console.log(total)
