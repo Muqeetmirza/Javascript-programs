7dbd0d9923c34f71e03f4316d8c40c0a9ce9d2a1
Muqeetmirza <a.muqet9405@gmail.com>
2022-05-15 04:32:59 +0500

    Assignment operation

---
+++ assignment.operators.js
@@ -0,0 +1,14 @@
+//assignments are used to perform operation with in 2 variables
+//we dont need a 3rd variable in it to store values
+let x=43,y=4;
+x+=y
+console.log(x)
+x-=y
+console.log(x)
+x/=y
+console.log(x)
+x%=y
+console.log(x)
+x**=y
+console.log(x)
+//same as we use in arthematic section but its more short and easy
