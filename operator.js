91f800f6edcdcaddf75c3de0f9633ec9256d06e2
Muqeetmirza <a.muqet9405@gmail.com>
2022-05-14 13:00:10 +0500

    arthematcal operations

---
+++ operator.js
@@ -0,0 +1,28 @@
+//simply the operators are + - * ** / % etc
+//work as simple  operations
+const x=10, y=43
+var z
+z=x+y;
+console.log(z);
+z=x-y;
+console.log(z);
+z=x/y;
+console.log(z);
+z=x*y;
+console.log(z);
+z=x%y;
+console.log(z);
+//we can also add text values throug it like
+let a="ali", b=" haider",c="M.",d;
+d=c+a+b;
+console.log(d);
+
+
+
+//increment value
+let no=5,n2=5;
+var t=++no
+console.log(t);
+//for decrement
+var h=--n2
+console.log(h);
