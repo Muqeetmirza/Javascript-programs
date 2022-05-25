63c3c3b72ac7ef9b88f218b16f71f51fa6a445be
Muqeetmirza <a.muqet9405@gmail.com>
2022-05-25 13:03:12 +0500

    test1

--- testq1pt3.js
+++ testq1pt3.js
@@ -4,9 +4,11 @@ const obj={
     "email":"abc@gmail.com",
     "address":"gujrat",
     avg : function(){
+        //pass a function for avg
         let mark=45+50+23+54+54+54;
         let average=mark/6;
         return average;
+        //after return the value is in avg
     }
 };
 console.log(obj.avg());
